48ebaa7d1a6c641cb624d5b290bf16fdf081a939
gitMaxim <nosuchperson@yandex.com>
2019-07-16 01:41:08 +0300

    Console web client app added

---
+++ ConsoleWebClient/ConsoleApp/CrmQueries.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace ConsoleApp
+{
+    class CrmQueries
+    {
+        public QueryExpression CreateFlatworkQueryExpression(Guid oppId)
+        {
+            var queryExpression = new QueryExpression("new_flatwork")
+            {
+                ColumnSet = new ColumnSet(false),
+                Criteria =
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression("opportunityid", ConditionOperator.Equal, oppId),
+                    }
+                },
+                LinkEntities =
+                {
+                    // opportunityid - предполагаемое название поля в карточке new_flatwork, где хранится id связанного opportunity
+                    // opportunityid - поле в карточке opportunity, где хранит своё id
+                    new LinkEntity("new_flatwork", "opportunity", "opportunityid", "opportunityid", JoinOperator.Inner)
+                    {
+                        Columns = new ColumnSet(false),
+                        LinkEntities =
+                        {
+                            // new_integratoin_1c - в этом поле вроде бы опечатка; должно быть new_integration_1c?
+                            new LinkEntity("opportunity", "new_integratoin_1c", "opportunityid", "ref_opportunityid", JoinOperator.Inner)
+                            {
+                                Columns = new ColumnSet(false),
+                                LinkCriteria =
+                                {
+                                    Conditions =
+                                    {
+                                        new ConditionExpression("sync_date", ConditionOperator.OlderThanXDays, 7)
+                                    }
+                                },
+                                EntityAlias = "new_integratoin_1c"
+                            }
+                        },
+                        EntityAlias = "opportunity"
+                    }                    
+                }
+            };
+
+            return queryExpression;
+        }
+
+        public FetchExpression CreateFlatworkFetchExpression(Guid oppId)
+        {
+            var query = $@"
+                <fetch mapping='logical'>
+                  <entity name='new_flatwork'>
+                    <filter type='and'>
+                      <condition attribute='opportunityid' operator='eq' value='{oppId}'/>
+                    </filter>
+                    <link-entity name='opportunity' from='opportunityid' to='opportunityid' link-type='inner' alias='opportunity'>
+                      <link-entity name='new_integratoin_1c' from='opportunityid' to='ref_opportunityid' link-type='inner' alias='new_integratoin_1c'>
+                        <filter type='and'>
+                          <condition attribute='sync_date' operator='olderthan-x-days' value='7'/>
+                        </filter>
+                      </link-entity>
+                    </link-entity>
+                  </entity>
+                </fetch>";
+
+            var fetchExpression = new FetchExpression(query);
+
+            return fetchExpression;
+        }
+    }
+}
---
+++ ConsoleWebClient/ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk.Query;
+using OrgServiceDataContract;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Error: only oppId required.");
+                return;
+            }
+
+            if (!Guid.TryParse(args[0], out Guid oppId))
+            {
+                Console.WriteLine("Error: couldn't parse passed oppId argument.");
+                return;
+            }
+
+            try
+            {
+                var crmQueries = new CrmQueries();
+
+                QueryExpression queryExpression = crmQueries.CreateFlatworkQueryExpression(oppId);
+                FetchExpression fetchExpression = crmQueries.CreateFlatworkFetchExpression(oppId);
+
+                string serviceAddress = ConfigurationManager.AppSettings["orgServiceAddress"];
+                int maxBufferSize = int.Parse(ConfigurationManager.AppSettings["maxBufferSize"]);
+                int timeoutSeconds = int.Parse(ConfigurationManager.AppSettings["timeoutSeconds"]);
+
+                TimeSpan defaultTimeSpan = new TimeSpan(0, 0, timeoutSeconds);
+
+                var binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport)
+                {
+                    MaxReceivedMessageSize = maxBufferSize,
+                    MaxBufferSize = maxBufferSize,
+                    CloseTimeout = defaultTimeSpan,
+                    SendTimeout = defaultTimeSpan,
+                    ReceiveTimeout = defaultTimeSpan,
+                    Security =
+                    {
+                        Transport =
+                        {
+                            ClientCredentialType = HttpClientCredentialType.None
+                        }
+                    }
+                };
+
+                var address = new EndpointAddress(serviceAddress);
+                var orgServiceFactory = new ChannelFactory<IOrgServiceDataContract>(binding, address);
+                IOrgServiceDataContract orgService = orgServiceFactory.CreateChannel();
+
+                var result = orgService.RetrieveMultiple(queryExpression);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
